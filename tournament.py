dbe71eec0b4e5d4a57ec946ab751bb6909b0a268
Heather Phelps <heather.phelps@gmail.com>
2017-03-14 11:05:29 -0400

    Major overhaul per review
    Created views in SQL, re-wrote majority of methods

--- tournament.py
+++ tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -19,10 +19,6 @@ def deleteMatches():
     c.execute("DELETE FROM matches")
     db.commit()
 
-    # set all players num_matches to 0
-    c.execute("UPDATE players SET num_matches = 0 WHERE num_matches != 0;")
-    db.commit()
-
     db.close()
 
 
@@ -40,15 +36,16 @@ def countPlayers():
     db = connect()
     c = db.cursor()
     c.execute("SELECT count(*) FROM players")
-    for row in c.fetchall():
-        tt = row[0]
+    # for row in c.fetchall():
+    #     tt = row[0]
+    tt = c.fetchone()[0]
     db.close()
     return tt
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
 
@@ -59,7 +56,9 @@ def registerPlayer(name):
     c = db.cursor()
     player = bleach.clean(name)
     player = bleach.linkify(player)
-    c.execute("INSERT INTO players (name) VALUES (%s)", (player,))
+    query = "INSERT INTO players (name) VALUES (%s)"
+    params = (player, )
+    c.execute(query, params)
     db.commit()
     db.close()
 
@@ -77,32 +76,16 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    # t = ()
+    # o = list(t)
+
     db = connect()
     c = db.cursor()
-    c.execute("""select players.name, players.ID, foo.numw,
-        players.num_matches
-        FROM (select players.ID,
-        count(matches.winner) as numW
-        from players left join matches on players.ID = matches.winner
-        group by players.ID order by numW) as foo
-        join players on players.ID = foo.id;""")
-    # for row in c.fetchall():
-    #     m = row[2] + row[3]
-    #     t = ({'id': str(row[1]), 'name': str(row[0]), 'wins': row[2],
-    #           'matches': m})
-    t = ()
-    o = list(t)
-    for row in c.fetchall():
-        j = (str(row[1]), str(row[0]), row[2], row[3])
-        o.append(j)
-
-    t2 = tuple(o)
-
-    # posts = ({'id': str(row[1]), 'name': str(row[0]), 'wins': row[2],
-    #           'matches': row[3]}
-             
+    query = "select * from Standings"
+    c.execute(query)
+    results = c.fetchall()
     db.close()
-    return t2
+    return results
 
 
 def reportMatch(winner, loser):
@@ -118,70 +101,34 @@ def reportMatch(winner, loser):
     data = (winner, loser)
     c.execute(sql, data)
     db.commit()
-
-    newm = 0
-    # find out number of matches for winner ID
-    # sql = "SELECT num_matches from players where id = %s;"
-    #        SELECT num_matches from players where id = 129;
-    # c.execute(sql, winner)
-    # c.execute("SELECT num_matches from players where id = %s;"), (winner, )
-    c.execute("SELECT num_matches from players where id=%s;", (winner,))
-    for row in c.fetchall():
-        newm = int(row[0]) + 1
-    # increment the number
-    sql = "UPDATE players SET num_matches = %s WHERE id = %s;"
-    data = (newm, winner)
-    c.execute(sql, data)
-    db.commit()
-
-    # find out number of matches for loser ID
-    c.execute("SELECT num_matches from players where id=%s;", (loser,))
-    # increment the number
-    for row in c.fetchall():
-        newm = row[0] + 1
-    sql = "UPDATE players SET num_matches = %s WHERE id = %s;"
-    data = (newm, loser)
-    c.execute(sql, data)
-    db.commit()
-
     db.close()
 
 
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
         name1: the first player's name
         id2: the second player's unique id
         name2: the second player's name
-    """
-    db = connect()
-    c = db.cursor()
-    c.execute("""select players.name, players.ID, foo.numw,
-        players.num_matches
-        FROM (select players.ID,
-        count(matches.winner) as numW
-        from players left join matches on players.ID = matches.winner
-        group by players.ID order by numW) as foo 
-        join players on players.ID = foo.id order by numw desc;""")
+    # """
 
+    standings = playerStandings()  # ID, name, wins, matches
     t = ()  # empty tuple
     o = list(t)  # master list
     w = []  # empty list
-    for row in c.fetchall():
-        w.append(str(row[1]))
-        w.append(str(row[0]))
+    for u in standings:
+        w.append(u[0])  # id
+        w.append(u[1])  # name
         if len(w) % 4 == 0:
-            o.append(w)
+            p = tuple(w)
+            o.append(p)
             w = []
-    t = tuple(o)
-
-    db.close()
-    return t
+    return o
--- tournament.sql
+++ tournament.sql
@@ -7,21 +7,42 @@
 -- these lines here.
 
 
+DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 
 \c tournament
 
 -- works
 CREATE TABLE players (
-        ID serial,
-        name text,
-        num_matches int DEFAULT 0
+        ID serial PRIMARY KEY,
+        name text
 );
 
 
 CREATE TABLE matches (
-        match_id serial,
-        winner int,
-        loser int
+        match_id serial PRIMARY KEY,
+        winner int REFERENCES players (ID),
+        loser int REFERENCES players (ID)
 );
 
+CREATE VIEW Losses AS
+    SELECT players.ID, count(matches.loser) as losses
+    FROM players LEFT JOIN matches on players.ID = matches.loser 
+    group by players.ID;
+
+CREATE VIEW Wins AS
+    SELECT players.ID, count(matches.winner) as wins
+    FROM players LEFT JOIN matches on players.ID = matches.winner 
+    group by players.ID;
+
+CREATE VIEW total_matches AS
+	SELECT Wins.ID, Wins.wins + Losses.losses AS tmatches 
+	FROM Wins, Losses
+	WHERE Wins.ID = Losses.ID;
+
+CREATE VIEW Standings AS
+	SELECT Wins.ID as ID, players.name, Wins.wins, 
+	total_matches.tmatches FROM Wins
+	JOIN players ON players.ID = Wins.ID
+	JOIN total_matches ON total_matches.ID = Wins.ID
+	ORDER BY Wins.wins DESC;
